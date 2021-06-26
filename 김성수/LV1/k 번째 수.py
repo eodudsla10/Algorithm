e2dd8aa9427f44097dc240e501afbec56e17e18a
kssgit <76714467+kssgit@users.noreply.github.com>
2021-06-26 10:55:53 +0900

    Update k 번째 수.py

--- 김성수/LV1/k 번째 수.py
+++ 김성수/LV1/k 번째 수.py
@@ -14,4 +14,15 @@ def solution(array , commands):
 array = [1, 5, 2, 6, 3, 7, 4]
 commands =[[2, 5, 3], [4, 4, 1], [1, 7, 3]]
 
+## 다른 사람 풀이
+def solution2(array, commands):
+    return list(
+        map(lambda x:
+        sorted(array[x[0]-1:x[1]])[x[2]-1], 
+        ##  commands = [[2, 5, 3], [4, 4, 1], [1, 7, 3]]
+        # x =[2,5,3]
+        ## array[2-1:5] = [5,2,6,3]  -> [2,3,5,6] [2] 
+        commands)
+        )
+
 print(solution(array,commands))
