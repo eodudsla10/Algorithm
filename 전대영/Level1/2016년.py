67c63443f99ad7217515f604a0c5e818af10cc16
jeondeayeong <eodudsla10@naver.com>
2021-07-01 14:37:47 +0900

    210701 update

---
+++ 전대영/Level1/2016년.py
@@ -0,0 +1,10 @@
+def solution(a, b):
+    day =['FRI','SAT','SUN','MON','TUE','WED','THU']
+    month =[31,29,31,30,31,30,31,31,30,31,30,31]
+    k = sum(month[:a-1]) + b-1
+    return day[k %7]
+    
+
+a =5
+b=24
+print(solution(a,b))
