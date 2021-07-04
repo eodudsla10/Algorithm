97469a22cf86e24334c29321a18eaabe6b923826
kssgit <76714467+kssgit@users.noreply.github.com>
2021-07-04 12:23:31 +0900

    Study 0704

--- 김성수/LV1/핸드폰 번호 가리기.py
+++ 김성수/LV1/핸드폰 번호 가리기.py
@@ -7,7 +7,22 @@ def solution(phone_number):
         else:
             result+=i
         c +=1
- 
+
     return result[::-1]
 
 print(solution("027778888"))
+
+## 
+def solution2(phone_number):
+    # 문자열을 list로 형변환
+    list_number =list(phone_number)
+    # 마지막 4개의 숫자를 제외한 나머지 문자열 변환 
+    for i in range(0,len(phone_number)-4,1):
+        list_number[i] = '*'
+    
+    ## .join 함수를 이용해 리스트를 문자열로 형변환
+    result = "".join(list_number)
+    return result
+
+
+print(solution2("0144444444444"))
