b16dd41a54342e694d7a9215f1cff0f3b40bfd78
kssgit <76714467+kssgit@users.noreply.github.com>
2021-07-09 12:18:16 +0900

    Update 다익스트라_구현_방법.py

--- 김성수/알고리즘/다익스트라_구현_방법.py
+++ 김성수/알고리즘/다익스트라_구현_방법.py
@@ -25,6 +25,7 @@ def get_smallest_node():
     min_value = INF
     index = 0 # 가장 최단 거리가 짧은 노드 ( 인덱스)
     for i in range(1,n+1):
+        # 방문하지 않은 노드 중에서 
         if distance[i] < min_value and not visited[i]:
             min_value = distance[i]
             index = i
