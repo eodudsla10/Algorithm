0860d86de4ee8e2705d69b88371752c0e91834d7
kssgit <76714467+kssgit@users.noreply.github.com>
2021-07-10 01:23:09 +0900

    Create 숫자 문자열과 영단어.py

---
+++ 김성수/프로그래머스 LV1/숫자 문자열과 영단어.py
@@ -0,0 +1,19 @@
+def solution(s):
+    answer = 0
+    numbers = ['zero','one','two','three','four','five','six','seven','eight','nine']
+    num = ''
+    result = ''
+    for i in s :
+        if i.isalpha():
+            num+=i
+        else:
+            result+=i
+        if num in numbers:
+            result+=str(numbers.index(num))
+            num=''
+    return int(result)
+
+
+s = "one4seveneight" 
+
+print(solution(s))
