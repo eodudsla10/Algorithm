0dd3ded58def4a41a704cfed659f7a7fac8cbcbe
kssgit <76714467+kssgit@users.noreply.github.com>
2021-07-14 12:25:01 +0900

    Study 0714

---
+++ 김성수/프로그래머스 LV1/약수의 개수와 덧셈.py
@@ -0,0 +1,42 @@
+def solution(left, right):
+    answer = 0
+    for i in range(left,right+1):
+        count = 0
+        n = i 
+        약수 =i
+        while True:
+            if n%약수==0:
+                count+=1
+            if 약수 <=1:
+                break
+            약수 -=1
+        if count%2==0:
+            answer+=n
+        else:
+            answer-=n
+    return answer
+
+
+
+
+left = 13
+right = 17
+
+print(solution(left,right))
+
+####### 
+##다른 사람 풀이
+# 약수의 개수가 짝수개인지 홀수개인지는 제곱근을 구하면 알수 있다 
+import math
+
+def solution(left, right):
+    answer = 0
+    for i in range(left, right + 1, 1):
+        sqrt = math.sqrt(i)
+        if int(sqrt) == sqrt:
+            answer -= i
+        else:
+            answer += i
+
+    return answer
+
