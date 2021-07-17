97f142c8fb3e3ac76d7c914a45d5cba48dd57158
kssgit <76714467+kssgit@users.noreply.github.com>
2021-07-17 21:14:03 +0900

    Update 더 맵게.py

--- 김성수/프로그래머스 LV2/더 맵게.py
+++ 김성수/프로그래머스 LV2/더 맵게.py
@@ -1,28 +1,32 @@
 # 더 맵게
 #섞은 음식의 스코빌 지수 = 가장 맵지 않은 음식의 스코빌 지수 + (두 번째로 맵지 않은 음식의 스코빌 지수 * 2)
 
+import heapq
+
 def solution(scoville, K):
     answer = 0
-    mix = scoville[0]
-    i = 1
-    while mix < K:
-        answer +=1
-        if mix < scoville[i]:
-            mix += (scoville[i]*2)
-        else:
-            mix = scoville[i]+()
-        print(mix)
-        if mix > K:
-            break
-        if i >= len(scoville)-1:
+    # heap에 스코빌 리스트를 삽입
+    min_heap=[]
+    for s in scoville:
+        heapq.heappush(min_heap,s)
+    # min heap에서 값을 하나씩 빼면서 k와 비교후 작으면 섞는다
+    while True:
+        if len(min_heap) == 1 and min_heap[0] <K:
             answer = -1
             break
-        i +=1
+        min_s1 = heapq.heappop(min_heap)
+        if min_s1 < K:
+            answer+=1
+            min_s2 = heapq.heappop(min_heap)
+            mix_s = min_s1 +(min_s2*2)
+            heapq.heappush(min_heap,mix_s)
+        else:
+            break
     return answer
 
 
-scoville = [1, 2, 3, 9, 10, 12]
-k = 7
+scoville = [1, 1,1]
+k = 8
 print(solution(scoville,k))
 # 1 2 0 1
 # 3   2
