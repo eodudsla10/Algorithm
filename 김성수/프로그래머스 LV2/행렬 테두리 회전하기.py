7176a6caa07a81fc718c11696c7bd82f0f6a27e5
kssgit <76714467+kssgit@users.noreply.github.com>
2021-08-06 21:52:10 +0900

    Create 행렬 테두리 회전하기.py

---
+++ 김성수/프로그래머스 LV2/행렬 테두리 회전하기.py
@@ -0,0 +1,14 @@
+def solution(rows, columns, queries):
+    answer = []
+    return answer
+
+
+rows = 3
+columns = 3
+queries = [
+    [1,1,2,2],
+    [1,2,2,3],
+    [2,1,3,2],
+    [2,2,3,3]]
+
+print(solution(rows,columns,queries))
