0d4e7ea30edd728447ee7d388715df7cab33771f
전대영 <eodudsla10@naver.com>
2021-09-03 23:43:55 +0900

    210903 update

---
+++ 전대영/Baekjoon/JUN1712_손익분기점.java
@@ -0,0 +1,26 @@
+package com.day19;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+ 
+public class JUN1712_손익분기점 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+		StringTokenizer st = new StringTokenizer(br.readLine());
+ 
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+ 
+		if (C <= B) {
+			System.out.println("-1");
+		} 
+		else {
+			System.out.println((A/(C-B))+1);
+		}
+	}
+}
+ 
