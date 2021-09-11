516eda15bef19b38d2ff9cc6080aec72bc4456b0
전대영 <eodudsla10@naver.com>
2021-09-11 16:13:42 +0900

    210911 update

--- 전대영/Baekjoon/JUN1712_손익분기점.java
+++ 전대영/Baekjoon/JUN1712_손익분기점.java
@@ -4,23 +4,19 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
- 
+
 public class JUN1712_손익분기점 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
 		StringTokenizer st = new StringTokenizer(br.readLine());
- 
-		int A = Integer.parseInt(st.nextToken());
-		int B = Integer.parseInt(st.nextToken());
-		int C = Integer.parseInt(st.nextToken());
- 
-		if (C <= B) {
-			System.out.println("-1");
-		} 
-		else {
-			System.out.println((A/(C-B))+1);
-		}
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int t = Integer.parseInt(st.nextToken());
+		
+		if(t <=m)
+			System.out.println(-1);
+		else
+			System.out.println(n/(t-m)+1);
 	}
 }
  
